1bcd0b7928094754f68b8152265a5601a4f42d22
unknown <yuri.vodich@gmail.com>
2021-04-08 02:16:18 +0300

    objects tasks solving

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -116,36 +116,156 @@ function fromJSON(proto, json) {
  *  For more examples see unit tests.
  */
 
+
+function selectorBase(val, builder) {
+  this.value = val;
+  if (builder instanceof selectorBase) {
+    this.prevValue = builder.stringify();
+  } else {
+    this.prevValue = '';
+  }
+}
+function ElementSelector(val, builder) {
+  this.value = val;
+  if (builder instanceof selectorBase) {
+    this.prevValue = builder.stringify();
+  } else {
+    this.prevValue = '';
+  }
+}
+function IdSelector(val, builder) {
+  this.value = val;
+  if (builder instanceof selectorBase) {
+    this.prevValue = builder.stringify();
+  } else {
+    this.prevValue = '';
+  }
+}
+
+function ClassSelector(val, builder) {
+  this.value = val;
+  if (builder instanceof selectorBase) {
+    this.prevValue = builder.stringify();
+  } else {
+    this.prevValue = '';
+  }
+}
+
+function AttrSelector(val, builder) {
+  this.value = val;
+  if (builder instanceof selectorBase) {
+    this.prevValue = builder.stringify();
+  } else {
+    this.prevValue = '';
+  }
+}
+function PseudoClassSelector(val, builder) {
+  this.value = val;
+  if (builder instanceof selectorBase) {
+    this.prevValue = builder.stringify();
+  } else {
+    this.prevValue = '';
+  }
+}
+function PseudoElementSelector(val, builder) {
+  this.value = val;
+  if (builder instanceof selectorBase) {
+    this.prevValue = builder.stringify();
+  } else {
+    this.prevValue = '';
+  }
+}
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    const selector = new ElementSelector(value, this);
+    return selector;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    const selector = new IdSelector(value, this);
+    return selector;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    const selector = new ClassSelector(value, this);
+    return selector;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    const selector = new AttrSelector(value, this);
+    return selector;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    const selector = new PseudoClassSelector(value, this);
+    return selector;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    const selector = new PseudoElementSelector(value, this);
+    return selector;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    return {
+      stringify() {
+        return `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+      },
+    };
   },
 };
-
+function selectorsOrderError() {
+  throw new Error(/Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element/);
+}
+function multipleSelectorsError() {
+  throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+}
+selectorBase.prototype = cssSelectorBuilder;
+selectorBase.prototype.currentValueStr = function () {
+  return `${this.value}`;
+};
+selectorBase.prototype.stringify = function () {
+  return `${this.prevValue}${this.currentValueStr()}`;
+};
+ElementSelector.prototype = Object.create(selectorBase.prototype);
+ElementSelector.prototype.element = multipleSelectorsError;
+IdSelector.prototype = Object.create(selectorBase.prototype);
+IdSelector.prototype.currentValueStr = function () {
+  return `#${this.value}`;
+};
+IdSelector.prototype.id = multipleSelectorsError;
+IdSelector.prototype.element = selectorsOrderError;
+ClassSelector.prototype = Object.create(selectorBase.prototype);
+ClassSelector.prototype.currentValueStr = function () {
+  return `.${this.value}`;
+};
+ClassSelector.prototype.element = selectorsOrderError;
+ClassSelector.prototype.id = selectorsOrderError;
+AttrSelector.prototype = Object.create(selectorBase.prototype);
+AttrSelector.prototype.currentValueStr = function () {
+  return `[${this.value}]`;
+};
+AttrSelector.prototype.element = selectorsOrderError;
+AttrSelector.prototype.id = selectorsOrderError;
+AttrSelector.prototype.class = selectorsOrderError;
+PseudoClassSelector.prototype = Object.create(selectorBase.prototype);
+PseudoClassSelector.prototype.currentValueStr = function () {
+  return `:${this.value}`;
+};
+PseudoClassSelector.prototype.element = selectorsOrderError;
+PseudoClassSelector.prototype.id = selectorsOrderError;
+PseudoClassSelector.prototype.class = selectorsOrderError;
+PseudoClassSelector.prototype.attr = selectorsOrderError;
+PseudoElementSelector.prototype = Object.create(selectorBase.prototype);
+PseudoElementSelector.prototype.currentValueStr = function () {
+  return `::${this.value}`;
+};
+PseudoElementSelector.prototype.pseudoElement = multipleSelectorsError;
+PseudoElementSelector.prototype.element = selectorsOrderError;
+PseudoElementSelector.prototype.id = selectorsOrderError;
+PseudoElementSelector.prototype.class = selectorsOrderError;
+PseudoElementSelector.prototype.attr = selectorsOrderError;
+PseudoElementSelector.prototype.pseudoClass = selectorsOrderError;
 
 module.exports = {
   Rectangle,

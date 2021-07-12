6db563d8cc385f704a2dad1e2d19b73046327bfe
YVodzich <yuri.vodich@gmail.com>
2021-07-13 01:43:29 +0300

    conditions tasks solving

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -314,8 +314,14 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sumArrDigital = num;
+  while (sumArrDigital > 9) {
+    const arrDigital = sumArrDigital.toString().split('');
+    sumArrDigital = arrDigital.reduce((acc, cur) => +acc + +cur);
+    if (sumArrDigital < 10) break;
+  }
+  return sumArrDigital;
 }
 
 

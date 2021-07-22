07c3f43e85514d701bc1aa0f5559953625fb4745
YVodzich <yuri.vodich@gmail.com>
2021-07-23 02:21:54 +0300

    functions tasks solving

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -157,8 +157,11 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    args1.push(args2);
+    return fn(...args1.flat());
+  };
 }
 
 
